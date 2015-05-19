6a060e19dd7978082c7edfc9f6687497db2abe8b
shelly8525 <shelly8525@yahoo.com.tw>
2015-05-19 11:01:47 +0800

    main method, sort()和 printArray()呼叫

---
+++ src/ArraySort.java
@@ -0,0 +1,18 @@
+
+public class ArraySort {
+	public static void main(String[] args){
+		int m[][] = {{4,2},{1,7},{4,5},{1,2},{1,1},{4,1}};
+		
+		sort(m);
+		
+		printArray(m);
+	}
+	
+	public static void sort(int m[][]){
+		
+	}
+	
+	public static void printArray(int m[][]){
+		
+	}
+}
