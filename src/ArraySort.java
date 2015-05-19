28603e5dcef94e801f1c4f97f92c86176051e399
shelly8525 <shelly8525@yahoo.com.tw>
2015-05-19 11:09:50 +0800

    寫出sort方法

--- src/ArraySort.java
+++ src/ArraySort.java
@@ -9,7 +9,26 @@ public class ArraySort {
 	}
 	
 	public static void sort(int m[][]){
-		
+		for(int i = 0;i < m.length;i++){
+			double currentMin = m[i][0];
+			int currentMinIndex = i;
+			
+			for(int j = 0;j < m.length;j++){
+				if(currentMin > m[j][0] || (currentMin == m[j][0] && m[currentMinIndex][1] > m[j][1])){
+					currentMin = m[j][0];
+			        currentMinIndex = j;
+				}
+			}
+			
+			if (currentMinIndex != i) {
+		        int temp0 = m[currentMinIndex][0];
+		        int temp1 = m[currentMinIndex][1];
+		        m[currentMinIndex][0] = m[i][0];
+		        m[currentMinIndex][1] = m[i][1];
+		        m[i][0] = temp0;
+		        m[i][1] = temp1;
+		  }
+		}
 	}
 	
 	public static void printArray(int m[][]){
